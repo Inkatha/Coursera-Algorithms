cc764060cb409466b335758ea70915e537c9fe1f
Malik Inkatha <minkatha01@gmail.com>
2016-10-02 01:03:40 -0500

    Add descriptions to problems 1 and 2.

---
+++ course01/week02/Problem01/Fibonacci.cs
@@ -0,0 +1,34 @@
+/* 
+1  Problem: Small Fibonacci Number
+*/
+
+using System;
+
+namespace Fibonacci
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+           long inputAmount = Convert.ToInt64(Console.ReadLine()); 
+
+           long result = Fibonacci(inputAmount);
+
+            Console.WriteLine(result);
+        }
+
+        public static long Fibonacci(long n) 
+        {
+            long a = 0;
+            long b = 1;
+
+            for (int i = 0; i < n; i++) 
+            {
+                long temp = a;
+                a = b;
+                b = temp + a;
+            }
+            return a;
+        }
+    }
+}
