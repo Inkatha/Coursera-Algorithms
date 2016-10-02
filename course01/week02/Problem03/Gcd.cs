9f4eb44f2cfb025cc35a44a6aea02262ecf6ed2c
Malik Inkatha <minkatha01@gmail.com>
2016-10-02 01:02:49 -0500

    Homework assignments 2 and 3

---
+++ course01/week02/Problem03/Gcd.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace GreastestCommonDivisor
+{
+    class Program 
+    {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+
+            string[] userInput = input.Split(' ');
+
+            int first = Convert.ToInt32(userInput[0]);
+            int second = Convert.ToInt32(userInput[1]);
+
+            Console.WriteLine(ComputeGcd(first, second));
+        }
+
+        public static int ComputeGcd(int a, int b) {
+            int r = a % b;
+            while (r != 0) {
+                a = b;
+                b = r;
+
+                r = a % b;
+            }
+            return b;
+        }
+    }
+}
